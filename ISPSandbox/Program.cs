706d1dd79ac45b642ab98e413a402bf09393d0e1
Mitsiorovich <mitsiorisn@protonmail.com>
2025-09-16 21:36:19 +0300

    Solution

---
+++ ISPSandbox/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ISPExample
+{
+    public interface IBasicFeatures
+    {
+        void Print(string content);
+    }
+
+    public interface IAdvancedFeatures // could be two interfaces but enough to be ISP compliant
+    {
+        void Scan(string content);
+        void Fax(string content);
+    }
+
+    public class OldPrinter : IBasicFeatures
+    {
+        public void Print(string content) => Console.WriteLine("Printing: " + content);
+    }
+
+    public class ModernPrinter : IAdvancedFeatures, IBasicFeatures
+    {
+        public void Print(string content) => Console.WriteLine("Printing: " + content);
+        public void Scan(string content) => Console.WriteLine("Scanning: " + content);
+        public void Fax(string content) => Console.WriteLine("Faxing: " + content);
+    }
+}
