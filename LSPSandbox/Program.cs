f5744bfd8074040dc809ab3132690f3b8bab22a3
Mitsiorovich <mitsiorisn@protonmail.com>
2025-09-16 21:20:19 +0300

    Solution

---
+++ LSPSandbox/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LSPExample
+{
+    public abstract class Shape()
+    {
+        public abstract int GetArea();
+    }
+
+    public class Rectangle: Shape
+    {
+        public  int Width { get; set; }
+        public  int Height { get; set; }
+
+        public override int GetArea() => Width * Height;
+    }
+
+    public class Square : Shape
+    {
+        public int SideWidth { get; set; }
+        public override int GetArea() => SideWidth * SideWidth;
+    }
+}
