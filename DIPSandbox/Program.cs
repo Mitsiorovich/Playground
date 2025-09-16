5d84bc0b352a0b91e5d3224645d791c5a16761a9
Mitsiorovich <mitsiorisn@protonmail.com>
2025-09-16 21:59:36 +0300

    Solution DIP with DI and Strategy

---
+++ DIPSandbox/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DIPTask
+{
+    public interface IProcessor
+    {
+        public void Pay(decimal amount);
+    }
+    public class CreditCardProcessor: IProcessor
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Processing credit card payment: {amount}");
+        }
+    }
+
+    public class PayPalProcessor : IProcessor 
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Processing PayPal payment: {amount}");
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IProcessor _processor;
+        public PaymentService(IProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public void MakePayment(decimal amount)
+        {
+            _processor.Pay(amount); 
+        }
+    }
+}
