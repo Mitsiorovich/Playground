18295f9bc7b1ffcff670f9fef0a41e5c47555b1e
Mitsiorovich <mitsiorisn@protonmail.com>
2025-09-17 22:21:08 +0300

    TaskBoard app Domain

---
+++ TaskBoardApp/TaskBoard.Domain/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoard.Domain.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
---
+++ TaskBoardApp/TaskBoard.Domain/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoard.Domain.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime DueDate { get; set; }
+    }
+}
